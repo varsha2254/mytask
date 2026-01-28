d33a667ed282d105794ba3c4f45dc9cf5aa271d8
varsha2254 <saivarsh2220@gmail.com>
2026-01-28 10:23:17 +0530

    Create sript.js

---
+++ sript.js
@@ -0,0 +1,8 @@
+const toggle = document.getElementById("toggleTheme");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = 
+    document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
+
